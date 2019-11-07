5eccde136cf615c1a8963c6fd88568832bc9bad1
Andrew Allison <andrew.allison@zyrofiser.co.uk>
2019-11-07 17:06:04 +0000

    feat(kill): sort kill processes out

--- docker-commands.ps1
+++ docker-commands.ps1
@@ -1,5 +1,8 @@
 # list al images
-docker images ls
+# docker images ls
+
+# Kill filtered apps
+# docker ps -a -q --filter "name=container_name" --format="{{.ID}}" | ForEach-Object -Process {docker rm $_ -f}
 
 # Kill all apps
-docker ps -a -q --filter "name=container_name" --format="{{.ID}}" | ForEach-Object -Process {docker rm $_ -f}
+docker ps -a -q --format="{{.ID}}" | ForEach-Object -Process {docker rm $_ -f}
